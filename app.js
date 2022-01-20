c91f31ab978f6c3c84480a01107888763edc2686
Robin Roy <robin.rrt123gmail.com>
2022-01-20 18:07:45 +0530

    added ejs support

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+
+// Set 'views' directory for any views 
+// being rendered res.render()
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
+// Set view engine as EJS
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +22,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index.ejs');
+});
+
+// // about page
+app.get('/about', function(req, res) {
+  res.render('pages/about.ejs');
+});
+
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +53,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('pages/error.ejs');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
 });
 
 module.exports = router;
